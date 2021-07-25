c5330cdcbe69badc534f7053b11018cb45268c9b
ジョイボーイ <69261996+joyboy67@users.noreply.github.com>
2021-07-25 14:06:05 +0200

    Update afd.c

--- afd.c
+++ afd.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#incude <stdbool.h>
+#define uint unsigned int 
 
 typedef struct Etat Etat
 typedef struct Arc Arc
 typedef struct AFD AFD
 
-
 struct Etat {
-
-
 };
 
 struct Arc {
-
-
 };
 
 
 struct AFD {
-
-
 };
+
+
+  int main (uint argc, char *argv[]) {
+  
+    puts("------- AFD Start ---------");
+    return 0;
+  
+  };
+
