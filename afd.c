fa41107c761836a6abbc32c0eb94f1ed0b6f8c04
ジョイボーイ <69261996+joyboy67@users.noreply.github.com>
2021-07-25 14:03:05 +0200

    Create afd.c

---
+++ afd.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Etat Etat
+typedef struct Arc Arc
+typedef struct AFD AFD
+
+
+struct Etat {
+
+
+};
+
+struct Arc {
+
+
+};
+
+
+struct AFD {
+
+
+};
