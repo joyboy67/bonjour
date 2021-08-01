412f93d9e79c59ab8ac29e01dc2860f76d227534
ジョイボーイ <69261996+joyboy67@users.noreply.github.com>
2021-08-01 20:39:10 +0200

    Update mruby.c

--- mruby.c
+++ mruby.c
@@ -4,9 +4,9 @@
 
 int main(void) {
     mrb_state *mrb = mrb_open();
-    char code[] = "5.times { puts 'mruby is awesome!' }";
+    char code[] = "5.times { puts 'mruby c\'est super!' }";
 
-    printf("Executing Ruby code with mruby:\n");
+    printf("Traitement de code Ruby avec mruby:\n");
     mrb_load_string(mrb, code);
 
     mrb_close(mrb);
