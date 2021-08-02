f89ff2ed5f55c652306142675de7d9e16ac0f5c5
ジョイボーイ <69261996+joyboy67@users.noreply.github.com>
2021-08-02 17:30:49 +0200

    Update wireword.rb

--- wireword.rb
+++ wireword.rb
@@ -3,12 +3,12 @@ require 'gosu'
 # Gosu 
 
 class GameWindow < Gosu::Window
-	def initialize
-      super 576, 576
-      self.caption = 'Wireworld'
-      @board_manager = BoardManager.new
-      @mouse_button_pressed = false
-    end
+   def initialize
+     super 576, 576
+     self.caption = 'Wireworld'
+     @board_manager = BoardManager.new
+     @mouse_button_pressed = false
+   end
 end
 
 #
@@ -16,16 +16,15 @@ end
 class Wireworld
 
 @@Etat = {
-		 tete: ">",
-		 queue: "-",
-		 vide: " ",
-		 conduteur: "x"
+	tete: ">",
+	queue: "-",
+	vide: " ",
+	conduteur: "x"
 }.frozen
 
-
-	def initialize
-		puts "Bienvenu sur Wireworld!"
-	end
+   def initialize
+      puts "Bienvenu sur Wireworld!"
+   end
 
 end
 
