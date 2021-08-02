f552d6d1d525cd9a638f3162694fbfd4ea7ccca5
ジョイボーイ <69261996+joyboy67@users.noreply.github.com>
2021-08-02 17:28:27 +0200

    Update wireword.rb

--- wireword.rb
+++ wireword.rb
@@ -1,1 +1,32 @@
+require 'gosu'
 
+# Gosu 
+
+class GameWindow < Gosu::Window
+	def initialize
+      super 576, 576
+      self.caption = 'Wireworld'
+      @board_manager = BoardManager.new
+      @mouse_button_pressed = false
+    end
+end
+
+#
+
+class Wireworld
+
+@@Etat = {
+		 tete: ">",
+		 queue: "-",
+		 vide: " ",
+		 conduteur: "x"
+}.frozen
+
+
+	def initialize
+		puts "Bienvenu sur Wireworld!"
+	end
+
+end
+
+Wireworld.new
