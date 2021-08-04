2fff328a1247caabbb67ff9efd8a51ced50330ff
ジョイボーイ <69261996+joyboy67@users.noreply.github.com>
2021-08-04 15:26:28 +0200

    Update euclid.rb

--- euclid.rb
+++ euclid.rb
@@ -1,6 +1,6 @@
 
 def euclide_div(x,y)
-  unless (x.is_a? Integer) && (y.is_a? Integeger)
+  unless (x.is_a? Integer) && (y.is_a? Integer)
     raise "Attendu : 2 entier(s)"
   else
     [x / y, x % y]
